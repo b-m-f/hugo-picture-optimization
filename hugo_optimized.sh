506a5e252c1245f316e64ae02f6a1f36a5c019ac
Maximilian Ehlers <2843450+b-m-f@users.noreply.github.com>
2020-03-22 16:01:01 +0800

    Update hugo_optimized.sh
    Update wrong argument for exiftool and missing target for mogrify

--- hugo_optimized.sh
+++ hugo_optimized.sh
@@ -6,7 +6,7 @@ hugo
 
 if hash exiftool 2>/dev/null; then
 	# remove exif data on all images in new_images
-	exiftool --all= public/images*
+	exiftool -all= public/images*
 else
 	echo "Install perl-image-exiftool to optimize images"
 fi
@@ -14,7 +14,7 @@ fi
 if hash jpegoptim 2>/dev/null; then
 	for image in $(find public/images -regextype posix-extended -iregex ".*\.(jpeg|jpg)"); do
 		# resize to width 1400 only if bigger than that
-		mogrify -resize '1400>'
+		mogrify -resize '1400>' $image
 		# remove all metadata and try to optimize jpeg image to match the Maximum size defined above
 		jpegoptim --strip-all --size=$MAXIMUM_JPG_SIZE $image
 	done;
